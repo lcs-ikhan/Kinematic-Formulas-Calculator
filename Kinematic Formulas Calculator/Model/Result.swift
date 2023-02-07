d3dcfe629d64cd6b69ae14682db44050a5a0e6c2
Isaad Khan <IKhan@lcs.on.ca>
2023-02-07 09:58:56 -0500

    Added result Structure

---
+++ Kinematic Formulas Calculator/Model/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  Kinematic Formulas Calculator
+//
+//  Created by Isaad Khan on 2023-02-07.
+//
+
+import SwiftUI
+
+import Foundation
+
+struct Result: Identifiable {
+    let id = UUID()
+    let a: Double
+    let b: Double
+    let c: Double
+    let roots: String
+    
+}
+
+
+
