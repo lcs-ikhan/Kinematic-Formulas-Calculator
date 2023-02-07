40f9a073acd3841922f76a681494560e0b4db78b
Isaad Khan <IKhan@lcs.on.ca>
2023-02-07 09:29:31 -0500

    Initial Commit

---
+++ Kinematic Formulas Calculator/Kinematic_Formulas_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kinematic_Formulas_CalculatorApp.swift
+//  Kinematic Formulas Calculator
+//
+//  Created by Isaad Khan on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct Kinematic_Formulas_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
